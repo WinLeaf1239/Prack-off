fa15a7a376b3bddf0681ad22ab2cf0da7f4ed2a4
WinLeaf1239 <WinLeaf1239@tutamail.com>
2025-10-27 15:54:36 +0300

    Not to finish

--- x = list(range(0, 10_000_000)).py
+++ x = list(range(0, 10_000_000)).py
@@ -1,21 +1,42 @@
 x = list(range(0, 10_000_000))
 iskomoe = 9_247_236
 is_found = False
-for i in range (0,len(x), 2**i): 
+index = 0
+jump = 1
+for i in range (0,len(x), jump): 
+    print(f"{jump}")
     jump = 2**i
     if iskomoe < x[i]:
+        start = i - jump
+        end = i
         while is_found is False :
-            mid = (i - (i-jump) ) // 2
-            if
+            mid = (end - (start) ) // 2
+            if x[mid] == iskomoe:
+                index = mid
+                is_found = True
+                break
+            elif x[mid] < iskomoe:
+                end=mid
+            else:
+                start=mid
+    if is_found :
+        break
 """        for j in range(i-jump,i):
             if x[j] == iskomoe :
                 index = j
                 is_found=True
                 break """
-    if x[index] == iskomoe :
+start = i - jump
+end = i
+while is_found is False :
+    mid = (end - (start) ) // 2
+    if x[mid] == iskomoe:
+        index = mid
+        is_found = True
         break
-for j in range(i,len(x),2**i): #Линейный поиск последнего куска, он будет меньше чем прыжок
-    if x[j] == iskomoe :
-        index = j
-        is_found=True
-        break
+    elif x[mid] < iskomoe:
+        end=mid
+    else:
+        start=mid
+
+if is_found 
